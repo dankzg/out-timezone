39d0898898378ca6566dd14d3f8376076db97a2b
dank.zheng <dank.zheng@mplus.tech>
2023-08-30 14:11:51 +0800

    add watch options

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,12 @@
  * @Author: dank.zheng
  * @Date: 2023-08-19 07:23:25
  * @LastEditors: dank.zheng
- * @LastEditTime: 2023-08-22 16:34:33
+ * @LastEditTime: 2023-08-30 09:24:32
 -->
 <script setup lang="ts">
 import UserInfos from '../components/UserInfos.vue'
 import ResultList from '../components/ResultList.vue'
-import { ref } from 'vue'
+import { ref, watch, nextTick } from 'vue'
 import { timezoneStore } from '@/stores/timezoneDatas'
 import { useJsonTimezoneToCountryStore } from '@/stores/jsonTimezoneToCountry'
 import { useJsonTimezoneStore } from '@/stores/jsonTimezone'
@@ -25,11 +25,22 @@ const tsStore = timezoneStore()
 tsStore.initData()
 
 const showResult = ref(false)
+
+watch(showResult, (value) => {
+  if (value) {
+    nextTick(() => {
+      const targetDiv = document.getElementById('targetDiv')
+      if (targetDiv) {
+        targetDiv.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      }
+    })
+  }
+})
 </script>
 
 <template>
   <UserInfos v-model:showResult="showResult" />
-  <div v-if="showResult">
+  <div v-if="showResult" ref="targetDiv">
     <n-divider dashed>{{ $t('home.result_title') }}</n-divider>
     <ResultList />
   </div>
